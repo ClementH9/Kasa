ab31a2554a18f95af6275b201c1c86578db46b16
ClementH9 <henot.clement@gmail.com>
2023-11-14 23:17:52 +0100

    création pages error et logement

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
 import Header from './components/Header/header'
+import About from '../pages/about/about'
+import Logement from './pages/logement/logement'
+import Error from './pages/error/error'
 
 ReactDOM.render(
   <React.StrictMode>
