5f21ad45ac5047975ff86f8e984890c430d2d6c1
ClementH9 <henot.clement@gmail.com>
2023-11-17 18:45:15 +0100

    modif mentor 17/11

--- kasa/src/components/Header/header.jsx
+++ kasa/src/components/Header/header.jsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
+import { Link } from 'react-router-dom';
+import React from 'react';
+import logo from './Assets/Images/logo-kasa';
+import 'App.css';
  
 function Header() {
     return (
-        <img className='Logo' alt="Logo" src="Logo"/>
         <nav>
+            <img className="logo" src={logo} alt="logo de l'agence kasa" />
             <Link to="/">Accueil</Link>
             <Link to="/about">A propos</Link>
         </nav>
