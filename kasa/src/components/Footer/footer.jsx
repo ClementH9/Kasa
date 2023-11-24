c30eac19bd23fc0aedb1681c7e3cf4805c75fcdf
ClementH9 <henot.clement@gmail.com>
2023-11-24 13:47:07 +0100

    fond noir footer

--- kasa/src/components/Footer/footer.jsx
+++ kasa/src/components/Footer/footer.jsx
@@ -1,9 +1,11 @@
 import logo from "../../Assets/Images/logo-footer.svg";
+import fondnoir from "../../Assets/Images/fond-noir-banner.png"
 
 export default function Footer() {
 	return (
 		<footer className="foot-container">
 			<img src={logo} alt="logo de kasa" />
+            <img src={fondnoir} alt="fond noir" />
 			<p className="foot-container__text">© 2020 Kasa. All rights reserved</p>
 		</footer>
 	);
