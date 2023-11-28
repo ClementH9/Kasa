91c3952fb25912a40270095b393251537a94c5bf
ClementH9 <henot.clement@gmail.com>
2023-11-28 16:27:33 +0100

    début card/gallery

--- kasa/src/pages/home/home.jsx
+++ kasa/src/pages/home/home.jsx
@@ -1,29 +1,22 @@
 import logo from '../../Assets/Images/logo.svg';
 import '../../Assets/css/App.css';
 import Banner from '../../components/Banners/home-banner';
+import Card from '../../components/Card/card'
 
 function App() {
   return (
     <>
       <Banner />
     </>
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Homepage
-        </a>
-      </header>
-    </div>*/
-  );
+    <div>
+        {logementCard.map((logement, logement.json) => (
+            <Card
+                cover={`${profile.name}-${index}`}
+                title={logement.title}
+            />
+        ))}
+    </div>
+  )
 }
 
 export default App;
