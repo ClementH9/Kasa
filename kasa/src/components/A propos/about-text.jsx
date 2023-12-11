a6c5dcbbf54260f10ab088f399344261662aa260
ClementH9 <henot.clement@gmail.com>
2023-12-11 15:10:49 +0100

    animation collapse

--- kasa/src/components/A propos/about-text.jsx
+++ kasa/src/components/A propos/about-text.jsx
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
-const Collapse = ({title, content}) => {
-    const [open, setOpen] = useState(false)
-    const iconToShow = open ? faAngleUp : faAngleDown
+const Collapse = ({ title, content }) => {
+  const [open, setOpen] = useState(false);
+  const iconToShow = open ? faAngleUp : faAngleDown;
 
-    return (
-        <div className="collapse-container">
-            <button onClick={() => setOpen(!open)} className="collapse-title">
-                {title}
-                <FontAwesomeIcon icon={iconToShow} />
-            </button>
-            <div className={`collapse-content ${!open ? "hide" : "show"}`}>
-                {content.split("\n").map((line, index) => (
-                    <p key={index}>{line}</p>
-                )
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="collapse-container">
+      <button onClick={() => setOpen(!open)} className="collapse-title">
+        {title}
+        <FontAwesomeIcon icon={iconToShow} />
+      </button>
+      <div
+        className={`collapse-content ${!open ? 'hide' : 'show'}`}
+        style={{ maxHeight: !open ? '0' : '1000px' }} // Ajoutez cette ligne
+      >
+        {content.split('\n').map((line, index) => (
+          <p key={index}>{line}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Collapse;
