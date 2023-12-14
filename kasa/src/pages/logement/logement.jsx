bc82acd993396e009133904346025ced8a0fe12d
ClementH9 <henot.clement@gmail.com>
2023-12-14 17:28:31 +0100

    contenu page logement

--- kasa/src/pages/logement/logement.jsx
+++ kasa/src/pages/logement/logement.jsx
@@ -1,25 +1,32 @@
-import logo from '../../Assets/Images/logo.svg';
+import logement from '../../Assets/logement.json';
 import '../../Assets/css/App.css';
+import { useParams } from 'react-router-dom';
 
-function Logement() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Logement
-        </a>
-      </header>
+function Appartement() {
+  let { id } = useParams();
+  const targetLogement = logement.find((data) => data.id==id);
+  if (targetLogement) {
+    
+    const hostNameArray = targetLogement.host.name.split(' ');
+    const hostFirstName = hostNameArray[0];
+    const hostLastName = hostNameArray.slice(1).join(' ');
+    return (
+    <div className="infos-logement font-text">
+      <img className="cover-logement" src={targetLogement.cover} alt="cover"></img>
+      <h2 className="title-logement">{targetLogement.title}</h2>
+      <div className="location-logement">{targetLogement.location}</div>
+      <div className="tags-logement">{targetLogement.tags}</div>
+      <div className="host-name-logement">{hostFirstName}
+          <br />
+          {hostLastName}</div>
+      <img className="host-picture-logement" src={targetLogement.host.picture} alt="host"></img>
+      <div className="rating-logement">{targetLogement.rating}</div>
+      <div className="desc-logement">{targetLogement.description}</div>
+      <div className="equipments-logement">{targetLogement.equipments}</div>
     </div>
-  );
+    
+    )
+  }
 }
 
-export default Logement;
+export default Appartement;
