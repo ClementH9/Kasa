8fee1c7528490b8688ccf809e98dd473ee20464d
ClementH9 <henot.clement@gmail.com>
2024-01-05 16:50:55 +0100

    CSS fini

--- kasa/src/pages/logement/logement.jsx
+++ kasa/src/pages/logement/logement.jsx
@@ -14,19 +14,6 @@ function Appartement() {
     const hostFirstName = hostNameArray[0];
     const hostLastName = hostNameArray.slice(1).join(' ');
     return (
-    /*<div className="infos-logement font-text">
-      <img className="cover-logement" src={targetLogement.cover} alt="cover"></img>
-      <h2 className="title-logement">{targetLogement.title}</h2>
-      <div className="location-logement">{targetLogement.location}</div>
-      <div className="tags-logement">{targetLogement.tags}</div>
-      <div className="host-name-logement">{hostFirstName}
-          <br />
-          {hostLastName}</div>
-      <img className="host-picture-logement" src={targetLogement.host.picture} alt="host"></img>
-      <div className="rating-logement">{targetLogement.rating}</div>
-      <div className="desc-logement">{targetLogement.description}</div>
-      <div className="equipments-logement">{targetLogement.equipments}</div>
-    </div>*/
     <div className="infos-logement font-text">
       <Carrousel pictures={targetLogement.pictures}/>
       <div className ="logement-top">
